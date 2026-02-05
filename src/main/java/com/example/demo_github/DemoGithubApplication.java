3501e1f27133a7bc737bd7fd5d38aa848a916bdc
Ulrich Vianney FOKAM <angevianney1715@gmail.com>
2026-02-05 15:57:48 +0100

    Add a REST Controller

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo_github;
+package com.example.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+
 public class DemoGithubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
+		SpringApplication.run(DemoGithubApplication.class, arg);
+
+		 
 	}
+
+}
+
+
+@RestController
+class HellowordController {
+
+	@GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
 
 }
